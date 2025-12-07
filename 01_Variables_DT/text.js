ffc05d747d5be9cb9e4f8949e03b0d656298856e
Rahul Patil <sudhakarpatil1247@gmail.com>
2025-12-07 14:32:25 +0000

    variables

--- 01_Variables_DT/text.js
+++ 01_Variables_DT/text.js
@@ -1,1 +1,21 @@
-console.log("HI I am Rahul")
+const accountId = 145536
+let accountEmail = "rahul234@gmai.com"
+var accountPassword = "234571"
+acountCity = "Yadur" 
+
+// accountId = 2 // not allowed
+accountEmail = "rakesh11@gmail.com"
+accountPassword = "12486654"
+acountCity = "BGM"
+
+
+// console.log(accountEmail);
+
+/*
+prefer not to use var
+because of issues in block scope and functional scope
+
+*/
+
+console.table([accountId, accountEmail, accountPassword, acountCity])
+
