028484d33b825850ac04f88bb77b4e7c7cbad470
Rahul Patil <sudhakarpatil1247@gmail.com>
2025-12-10 00:25:53 +0000

    converstion of data types

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,33 @@
+// let score ="50"; 
+// let score ="50dcrs"; 
+// let score = null; 
+// let score = true; 
+let score = "Rahul"; 
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc => NaN"
+// true => 1; false => 0; 
+
+let isLoggedIn = "Rahul"; 
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 =>  false;
+// "" => true;
+// " " & "Rahul" => true;
+
+let someNumber = 45;
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
