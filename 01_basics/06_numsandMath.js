2c647b02f142aa054fca2a14a73b8bf23e415807
Rahul Patil <sudhakarpatil1247@gmail.com>
2025-12-13 14:13:35 +0000

    nums and maths

---
+++ 01_basics/06_numsandMath.js
@@ -0,0 +1,36 @@
+const score = 400;
+  
+const balance = new Number(100);
+
+// console.log(balance);   
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.4534
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//************************** Maths *****************************
+
+// console.log(Math);
+// console.log(Math.abs(-3));
+// console.log(Math.round(5.5));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(1, 3, 4, 5, 6, 8));
+// console.log(Math.max(1, 3, 4, 5, 6, 8));
+
+
+console.log(Math.random())
+console.log((Math.random()*10) + 1)
+console.log(Math.floor(Math.random()*10) + 1)
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1))+  min)
+
